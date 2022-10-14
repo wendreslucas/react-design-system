9a9beb49a4aa7f17c4aef269b0797ce3c101424c
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-10-13 22:31:31 -0300

    commit inicial

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,44 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full text-xs focus-within:ring-2 ring-cyan-300">
+      {props.children}
+    </div>
+  );
+}
+
+TextInputRoot.displayName = "TextInput.Root";
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+  return <Slot className="w-6 h6 text-gray-400">{props.children}</Slot>;
+}
+
+export interface TextInputInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {}
+
+TextInputIcon.displayName = "TextInput.Icon";
+
+export function TextInputInput(props: TextInputInputProps) {
+  return (
+    <input
+      className="bg-transparent flex-1  text-gray-100 placeholder:text-gray-400 outline-none "
+      {...props}
+    />
+  );
+}
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
